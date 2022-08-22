7bb440053e80ae3e26d0b0221286326b16c8b010
teddyrugged <jakpaetuk@gmail.com>
2022-08-22 22:04:08 +0100

    database

--- Account/urls.py
+++ Account/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import(CustomTokenPairView,RegisterApiView)
+from rest_framework_simplejwt.views import (TokenObtainPairView,TokenRefreshView)
 
 
 urlpatterns = [
-    path("login/",CustomTokenPairView.as_view(),name="login"),
-    path("signup/",RegisterApiView.as_view(),name="signup")
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path("login/",CustomTokenPairView.as_view(),name="login"),
+    path("signup/",RegisterApiView.as_view(),name="signup"),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
